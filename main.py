3b9b24b5362ad02ea0338a3f8f1dfd8fb696136a
jenybo <86452879+JenyBo@users.noreply.github.com>
2023-04-20 14:36:11 +0700

    interface_update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ SCREEN_SIZE = (1280, 720)
 pygame.init()
 BASICFONT = pygame.font.Font('FiraCode-Retina.ttf',50)
 
-pygame.display.set_caption('8 Puzzle')
+pygame.display.set_caption('Puzzle')
 window_surface = pygame.display.set_mode(SCREEN_SIZE)
 background = pygame.Surface(SCREEN_SIZE)
 background.fill(pygame.Color(colors.BABY_BLUE))
@@ -26,7 +26,7 @@ def display_elements():
     #Elements
     ### Title Label
     pygame_gui.elements.ui_label.UILabel(manager=manager,
-                                        text="8-Puzzle Game",
+                                        text="Puzzle Game",
                                         relative_rect=pygame.Rect((540, 10), (300, 70)),
                                         object_id="#title_box"
                                         )
@@ -35,13 +35,13 @@ def display_elements():
 
 display_elements()
 ### solve button
-solve_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((1000, 640), (250, 45)),
+solve_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((1000, 350), (250, 45)),
                                              text='Solve Puzzle',
                                              manager=manager,
                                              object_id="#solve_btn")
 
 ### algorithmOptions DropDown
-dropdown_layout_rect = pygame.Rect((970, 600), (280, 35))
+dropdown_layout_rect = pygame.Rect((970, 295), (280, 35))
 algorithmOptions = ["A* (Manhatan Distance)","Best-First (Manhatan Distance)"]
 algorithmDropDown = pygame_gui.elements.UIDropDownMenu(options_list=algorithmOptions,
                                                        starting_option=algorithmOptions[1],
@@ -52,25 +52,25 @@ algorithmDropDown = pygame_gui.elements.UIDropDownMenu(options_list=algorithmOpt
 pygame_gui.elements.ui_label.UILabel(parent_element=algorithmDropDown,
                                      manager=manager,
                                      text="Heuristic Search:", # (pos-width,pos-height),(width,height)
-                                     relative_rect=pygame.Rect((800, 600), (170, 30)))
+                                     relative_rect=pygame.Rect((800, 295), (170, 30)))
 
 ### Final state input
-report_rect = pygame.Rect((1000, 210), (250, 30))
-Final_state = pygame_gui.elements.UITextEntryLine(relative_rect=report_rect,
-                                                  manager=manager)
+# report_rect = pygame.Rect((1000, 210), (250, 30))
+# Final_state = pygame_gui.elements.UITextEntryLine(relative_rect=report_rect,
+#                                                   manager=manager)
 
 ### Final state label
-pygame_gui.elements.ui_label.UILabel(parent_element=Final_state,
-                                     manager=manager,
-                                     text="Final State:", # (pos-width,pos-height),(width,height)
-                                     relative_rect=pygame.Rect((855, 210), (140, 30)))
+# pygame_gui.elements.ui_label.UILabel(parent_element=Final_state,
+#                                      manager=manager,
+#                                      text="Final State:", # (pos-width,pos-height),(width,height)
+#                                      relative_rect=pygame.Rect((855, 210), (140, 30)))
 
 ### set final state with button
-set_final_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((1000, 250), (250, 30)),
-                                                text='Set Final State',
-                                                manager=manager)
+# set_final_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((1000, 250), (250, 30)),
+#                                                 text='Set Final State',
+#                                                 manager=manager)
 ### shuffle button
-button_layout_rect = pygame.Rect((1000, 290), (250, 30))
+button_layout_rect = pygame.Rect((300, 150), (200, 40))
 shuffle_button = pygame_gui.elements.UIButton(relative_rect=button_layout_rect,
                                              text='Shuffle',
                                              manager=manager)
@@ -136,12 +136,12 @@ while is_running:
             if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                 if event.ui_element == shuffle_button:
                     puzzle.randomBlocks()
-                elif event.ui_element == set_final_button:
-                    if not puzzle.setBlocks(Final_state.get_text()):
-                        alert_label.set_text("Final state invalid!")
-                    else:
-                        alert_label.set_text("Final state valid!")
-                        puzzle.final_state = Final_state.get_text()
+                # elif event.ui_element == set_final_button:
+                #     if not puzzle.setBlocks(Final_state.get_text()):
+                #         alert_label.set_text("Final state invalid!")
+                #     else:
+                #         alert_label.set_text("Final state valid!")
+                #         puzzle.final_state = Final_state.get_text()
                 elif event.ui_element == info_button:
                     Info_msg = '<b>8-Puzzle Solver<br><br>Authors:</b><br>Mateus Mendonça Monteiro<br>Vinicius Santana Ramos'
                     # Information Box - Info
@@ -179,8 +179,8 @@ while is_running:
             elif event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
                 if event.ui_element == algorithmDropDown:
                     algorithm = event.text
-            elif event.user_type == pygame_gui.UI_TEXT_ENTRY_CHANGED and event.ui_element == Final_state:
-                print("")
+            # elif event.user_type == pygame_gui.UI_TEXT_ENTRY_CHANGED and event.ui_element == Final_state:
+            #     print("")
         manager.process_events(event)
         
         
